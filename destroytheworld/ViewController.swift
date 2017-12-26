8f16a53a0d9bb4059cd16666647d2d73c752a6d0
Swati Sharma <swatisharma@Swatis-MacBook-Air.local>
2017-12-26 16:22:34 +0530

    new proj

---
+++ destroytheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroytheworld
+//
+//  Created by Swati Sharma on 26/12/17.
+//  Copyright © 2017 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
